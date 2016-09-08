a1747dcf953c81425581a49304ae24566a2c6b78
Arun Ravindran <arun.ravindran@hotmail.co.nz>
2016-09-08 17:50:14 +1200

    typescript compiling

--- MSA Project/main.js
+++ MSA Project/main.js
@@ -1,18 +1,18 @@
 var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      var cityCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: map,
-            center: {lat: -34.397, lng: 150.644},
-            radius: 50000,
-            editable:true
-          });
-      }
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+    });
+    var cityCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map,
+        center: { lat: -34.397, lng: 150.644 },
+        radius: 50000,
+        editable: true
+    });
+}
